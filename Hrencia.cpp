03ed9e4587b701cb585d444f228e3d165ee578fc
JoseDeJesus002 <jose_1321124119@uptecamac.edu.mx>
2022-02-15 19:14:54 -0600

    Herencia

--- Hrencia.cpp
+++ Hrencia.cpp
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 using namespace std;
 
+//clase persona
+
 class persona{
 private:
 	string nombre;
@@ -11,14 +13,18 @@ public:
 	void mostrar();
 };
 persona::persona(string nombre_,int edad_)
+{
+
 	nombre=nombre_;
 	edad=edad_;
 }
 void persona::mostrar(){
+	cout<<"\n";
 	cout<<"nombre: "<<nombre<<endl;
 	cout<<"edad: "<<edad<<endl;
 }
 
+//clase empleado
 class empleado:public persona{
 private:
 	float sueldo;
@@ -34,37 +40,42 @@ void empleado::mostraremp(){
 	mostrar();
 	cout<<"sueldo: "<<sueldo<<endl;
 }
+
+//calse director
 class director:public persona{
 private:
 	string area;
 	int antiguedad;
+	int _sueldo;
 public:
-	director(string,int,string,int);
+	director(string,int,string,int,int);
 	void mostrar1();
 };
-director::director(string nombre_,int edad_,string area_,int antiguedad_):persona(nombre_,edad_){
+director::director(string nombre_,int edad_,string area_,int antiguedad_,int suel):persona(nombre_,edad_){
 	area=area_;
 	antiguedad=antiguedad_;
+	_sueldo=suel;
 }
 
 void director::mostrar1(){
+	mostrar();
 	cout<<"El area es: "<<area<<endl;
-	cout<<"La antiguedad es: "<<antiguedad<<" anios"<<endl;
+	cout<<"sueldo: "<<_sueldo<<endl;
+	cout<<"La antiguedad es: "<<antiguedad<<" anios"<<"\n"<<endl;
 }
 
-// director y empleado 
+//accion de herencias
 
 int main(){
 persona p1("Juan",32);
 p1.mostrar();
 
-empleado a1("Andres",53,18000);
+empleado a1("Andres",53,1800);
 a1.mostraremp();
 
-director a2("Mariana",49,"Ventas",15);
+director a2("Mariana",48,"Ventas",15,48000);
 a2.mostrar1();
 
-// 
 
 system("pause");
 	
