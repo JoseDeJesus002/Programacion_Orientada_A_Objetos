e62fce9ab9570e0c4829ad58e266f4e25f5bca42
JoseDeJesus002 <jose_1321124119@uptecamac.edu.mx>
2022-04-05 22:54:16 -0500

    boleto

--- boleto.cpp
+++ boleto.cpp
@@ -42,16 +42,16 @@ void boleto::descuento(){
 		cout<<"El boleto ninio sin descuento es: "<<precio2<<endl;
 		precio1=n*220*5/100;
 		precio3=n*220-precio1;
-		cout<<"\nLa fecha de compra es: 5/4/22\n\n hora 7:44\n\n precio con descuento es: "<<precio3<<endl;
+		cout<<"\nLa fecha de compra es: 5/4/22\n\n la hora de compra: 7:44\n\n precio con descuento es: "<<precio3<<endl;
 		break;
 	case 3:
-		cout<<"Ingresa la cantidad de boletos:";
+		cout<<"Ingresa la cantidad de boletos mayor: ";
 		cin>>n;
 		precio1=n*220;
 		cout<<"\nEl boleto adulto sin descuento es: "<<precio1<<endl;
 		precio1=n*220*30/100;
 		precio3=n*220-precio1;
-		cout<<"\nLa fecha de compra es: 5/4/22\n\n hora 7:44\n\n precio con descuento es: "<<precio3<<endl;
+		cout<<"\nLa fecha de compra es: 5/4/22\n\n la hora de compra: 7:44\n\n precio con descuento es: "<<precio3<<endl;
 		break;
 	}
 }
@@ -64,8 +64,22 @@ void boleto::mostrar(){
 	
 }
 int main(int argc, char *argv[]) {
-	boleto b("la bella durmiente","20","15:30");
+	int opc;
+
+	boleto b2 ("violinista en el tejado","30","22:00 pm");
+	boleto b("la bella durmiente","20","15:30 pm");
+	
+	cout<<"ingresa la opcion: ";cin>>opc;
+	switch(opc){
+	case 1:
+	b2.mostrar();
+	break;
+	case 2:
 	b.mostrar();
+	break;
+	}
+	system("pause");
+	system("cls");
 	return 0;
 }
 
